99726dc8cde327e19042060b20bbb13771d54a89
Timothy C. Wright <tcw321@gmail.com>
2015-07-20 19:17:57 -0400

    initial working version.  If the judge enters and leaves before all the immigrants, then the immigrants can get stuck.

---
+++ book_solution.cpp
@@ -0,0 +1,168 @@
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+std::mutex noJudge;
+std::mutex confirmed;
+std::mutex checked;
+std::mutex allSignedIn;
+std::mutex writeMessage;
+
+int immigrantsEntered=0;
+int immigrantsChecked=0;
+
+bool judge = 0;
+
+void enter()
+{
+
+}
+
+void checkIn()
+{
+
+}
+
+void sitDown()
+{
+
+}
+
+void swear()
+{
+
+}
+
+void getCertificate()
+{}
+
+void leave()
+{}
+
+void addImmigrant()
+{
+    noJudge.lock();
+    enter();
+    immigrantsEntered++;
+    {
+        std::lock_guard<std::mutex> lock(writeMessage);
+        std::cout << "immigrant entered\n";
+    }
+    noJudge.unlock();
+
+    checked.lock();
+    checkIn();
+    immigrantsChecked++;
+    {
+        std::lock_guard<std::mutex> lock(writeMessage);
+        std::cout << "immigrant checked in\n";
+    }
+    if ((judge == 1) && (immigrantsEntered == immigrantsChecked))
+        allSignedIn.unlock();
+    else
+        checked.unlock();
+
+    sitDown();
+    confirmed.lock();
+
+    swear();
+    getCertificate();
+
+    noJudge.lock();
+    leave();
+    noJudge.unlock();
+}
+
+void confirm()
+{
+
+}
+
+void addJudge()
+{
+    noJudge.lock();
+    checked.lock();
+
+    enter();
+    {
+        std::lock_guard<std::mutex> lock(writeMessage);
+        std::cout << "Judge entered\n";
+    }
+    judge = 1;
+
+    if (immigrantsEntered > immigrantsChecked)
+    {
+        checked.unlock();
+        allSignedIn.lock();
+    }
+    confirm();
+    while (immigrantsChecked !=0) {
+        confirmed.unlock();
+        immigrantsChecked--;
+    }
+    immigrantsEntered = 0;
+    immigrantsChecked = 0;
+
+    leave();
+    {
+        std::lock_guard<std::mutex> lock(writeMessage);
+        std::cout << "Judge left\n";
+    }
+    judge = 0;
+
+    checked.unlock();
+    noJudge.unlock();
+
+}
+
+void spectate()
+{}
+
+void addSpectator()
+{
+    noJudge.lock();
+    enter();
+    noJudge.unlock();
+    spectate();
+    leave();
+}
+
+int main()
+{
+    std::thread i1(addImmigrant);
+    std::thread i2(addImmigrant);
+    std::thread i3(addImmigrant);
+    std::thread i4(addImmigrant);
+    std::thread i5(addImmigrant);
+    std::thread i6(addImmigrant);
+    std::thread i7(addImmigrant);
+    std::thread j(addJudge);
+    //std::thread s1(addSpectator);
+    //std::thread s2(addSpectator);
+    //std::thread s3(addSpectator);
+    //std::thread s4(addSpectator);
+
+    std::this_thread::sleep_for(std::chrono::nanoseconds(900000));
+
+    i1.join();
+    i2.join();
+    i3.join();
+    i4.join();
+    i5.join();
+    i6.join();
+    i7.join();
+    j.join();
+    //s1.join();
+    //s2.join();
+    //s3.join();
+    //s4.join();
+    std::this_thread::sleep_for(std::chrono::nanoseconds(900000));
+
+    {
+        std::lock_guard<std::mutex> lock(writeMessage);
+        std::cout << "Done\n";
+    }
+
+}
+
