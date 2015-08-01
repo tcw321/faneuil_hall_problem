650fdfce404405310eafc122e334c9f1e80f4c8d
Timothy C. Wright <tcw321@gmail.com>
2015-08-01 12:09:36 -0400

    I think the state machine is working.

--- state_machine.cpp
+++ state_machine.cpp
@@ -15,7 +15,7 @@ std::mutex writeMessage;
 class Immigrant
 {
 public:
-    typedef enum {ENTER, CHECKIN, CONFIRMED, LEAVE} State;
+    typedef enum {ENTER, CHECKIN, SITDOWN, CONFIRMED, LEAVE} State;
 
     Immigrant(int id) : m_state(ENTER), m_immigrantDone(false), m_id(id)
     {}
@@ -26,56 +26,53 @@ public:
         while (m_immigrantDone == false) {
             switch (m_state) {
                 case ENTER:
-                    if (judgeIn == false)
+                    if ((judgeIn == false) && (allCheckedIn == false))
                     {
-                        if (immigrantsEntered == 0)
-                            allCheckedIn = false;
                         immigrantsEntered++;
                         m_state = CHECKIN;
                         {
                             std::lock_guard<std::mutex> lock(writeMessage);
                             std::cout << "IEnter " << m_id << "\n";
-                            std::cout << "entered " << immigrantsEntered << " checked " << immigrantsChecked << std::endl;
-                            std::cout << "judge in " << judgeIn << std::endl;
-                            std::cout << "all checked in " << allCheckedIn << std::endl;
                         }
                     }
                     std::this_thread::sleep_for(milliseconds(20));
                     break;
                 case CHECKIN:
                     immigrantsChecked++;
+                    m_state = SITDOWN;
+                    std::this_thread::sleep_for(milliseconds(3));
+                    break;
+                case SITDOWN:
                     if ((judgeIn == true) && (immigrantsChecked == immigrantsEntered)) {
                         allCheckedIn = true;
                     }
-                    m_state = CONFIRMED;
-                    {
+                    if (allCheckedIn == true) {
+                        m_state = CONFIRMED;
                         std::lock_guard<std::mutex> lock(writeMessage);
                         std::cout << "ICheckedin " << m_id << "\n";
-                        std::cout << "entered " << immigrantsEntered << " checked " << immigrantsChecked << std::endl;
-                        std::cout << "judge in " << judgeIn << std::endl;
-                        std::cout << "all checked in " << allCheckedIn << std::endl;
                     }
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
+
                 case CONFIRMED:
                     if (allCheckedIn == true) {
                         m_state = LEAVE;
                         {
                             std::lock_guard<std::mutex> lock(writeMessage);
                             std::cout << "IConfirmed " << m_id << "\n";
-                            std::cout << "judge in " << judgeIn << std::endl;
-                            std::cout << "all checked in " << allCheckedIn << std::endl;
                         }
                     }
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
                 case LEAVE:
                     m_immigrantDone = true;
+                    immigrantsEntered--;
+                    immigrantsChecked--;
+                    if (immigrantsEntered == 0)
+                        allCheckedIn = false;
                     {
                         std::lock_guard<std::mutex> lock(writeMessage);
                         std::cout << "ILeave " << m_id << "\n";
-                        std::cout << "judge in " << judgeIn << std::endl;
-                        std::cout << "all checked in " << allCheckedIn << std::endl;
                     }
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
@@ -89,18 +86,20 @@ private:
     int m_id;
 };
 
+
+
 class Judge
 {
 public:
     typedef enum { ENTER, CONFIRM, LEAVE} State;
 
-    Judge() : m_state(ENTER), m_judgeIn(true)
+    Judge() : m_state(ENTER), m_judgeDone(false)
     {}
 
     void run()
     {
         using namespace std::chrono;
-        while (m_judgeIn)
+        while (!m_judgeDone)
         {
             switch(m_state)
             {
@@ -110,45 +109,37 @@ public:
                     {
                         std::lock_guard<std::mutex> lock(writeMessage);
                         std::cout << "JEnter\n";
-                        std::cout << "judge in " << judgeIn << std::endl;
-                        std::cout << "all checked in " << allCheckedIn << std::endl;
-
                     }
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
                 case CONFIRM:
                     if (allCheckedIn == true) {
-                        allCheckedIn = false;
-                        immigrantsChecked = 0;
-                        immigrantsEntered = 0;
                         judgeIn = false;
                         m_state = LEAVE;
                         {
                             std::lock_guard<std::mutex> lock(writeMessage);
                             std::cout << "JConfirm\n";
-                            std::cout << "judge in " << judgeIn << std::endl;
-                            std::cout << "all checked in " << allCheckedIn << std::endl;
                         }
                     }
+                    if (immigrantsEntered == 0)
+                        m_state = LEAVE;
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
                 case LEAVE:
-                    m_judgeIn = false;
+                    judgeIn = false;
+                    m_judgeDone = true;
                     {
                         std::lock_guard<std::mutex> lock(writeMessage);
                         std::cout << "JLeave\n";
-                        std::cout << "judge in " << judgeIn << std::endl;
-                        std::cout << "all checked in " << allCheckedIn << std::endl;
                     }
                     std::this_thread::sleep_for(milliseconds(3));
                     break;
-
             }
         }
     }
 private:
     State m_state;
-    bool m_judgeIn;
+    bool m_judgeDone;
 };
 
 int main()
@@ -157,7 +148,6 @@ int main()
 
     immigrantsEntered = 0;
     immigrantsChecked = 0;
-    allCheckedIn = true;  // in case judge enters before any immigrants
     Immigrant immigrant1(1);
     std::thread t1(&Immigrant::run, immigrant1 );
 
@@ -184,8 +174,12 @@ int main()
 
     std::this_thread::sleep_for(seconds(1));
 
-    //std::thread j2(&Judge::run, judge);
-    //std::this_thread::sleep_for(seconds(1));
+    // clear any waiting immigrants
+    if (immigrantsEntered != 0) {
+       std::thread j2(&Judge::run, judge);
+        std::this_thread::sleep_for(seconds(1));
+        j2.join();
+    }
 
     t1.join();
     t2.join();
@@ -195,6 +189,6 @@ int main()
     t6.join();
     t7.join();
     j1.join();
-    //j2.join();
+
 
 }
